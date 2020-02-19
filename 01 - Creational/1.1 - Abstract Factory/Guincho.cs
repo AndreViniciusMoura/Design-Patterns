23970580a40e3fefc20e00d382f785d451a42418
andre.vinicius.moura@outlook.com <andre.vinicius.moura@outlook.com>
2020-02-19 20:35:13 -0300

    feat: Implementacao do projeto que apresenta os conceitos de Design-Patterns

---
+++ 01 - Creational/1.1 - Abstract Factory/Guincho.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace DesignPatterns.AbstractFactory
+{
+    // AbstractProduct   
+    public abstract class Guincho
+    {
+        protected Guincho(Porte porte)
+        {
+            Porte = porte;
+        }
+
+        public abstract void Socorrer(Veiculo veiculo);
+        public Porte Porte { get; set; }
+    }
+
+    // Produto Concreto
+    public class GuinchoPequeno : Guincho
+    {
+        public GuinchoPequeno() : base(Porte.Pequeno) { }
+
+        public override void Socorrer(Veiculo veiculo)
+        {
+            // Processo de socorro
+            Console.WriteLine("Socorrendo Carro Pequeno - Modelo " + veiculo.Modelo);
+        }
+    }
+
+    // Produto Concreto
+    public class GuinchoMedio : Guincho
+    {
+        public GuinchoMedio() : base(Porte.Medio) { }
+
+        public override void Socorrer(Veiculo veiculo)
+        {
+            // Processo de socorro
+            Console.WriteLine("Socorrendo Carro Medio - Modelo " + veiculo.Modelo);
+        }
+    }
+
+    // Produto Concreto
+    public class GuinchoGrande : Guincho
+    {
+        public GuinchoGrande() : base(Porte.Grande) { }
+
+        public override void Socorrer(Veiculo veiculo)
+        {
+            // Processo de socorro
+            Console.WriteLine("Socorrendo Carro Grande - Modelo " + veiculo.Modelo);
+        }
+    }
+
+    public class GuinchoCreator
+    {
+        public static Guincho Criar(Porte porte)
+        {
+            switch (porte)
+            {
+                case Porte.Pequeno:
+                    return new GuinchoPequeno();
+                case Porte.Medio:
+                    return new GuinchoMedio();
+                case Porte.Grande:
+                    return new GuinchoGrande();
+                default:
+                    throw new ApplicationException("Porte de Guincho desconhecido.");
+            }
+        }
+    }
+}
---
+++ 01 - Creational/1.1 - Abstract Factory/Veiculo.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace DesignPatterns.AbstractFactory
+{
+    // AbstractProduct  
+    public abstract class Veiculo
+    {
+        protected Veiculo(string modelo, Porte porte)
+        {
+            Modelo = modelo;
+            Porte = porte;
+        }
+
+        public string Modelo { get; set; }
+        public Porte Porte { get; set; }
+
+    }
+
+    public enum Porte
+    {
+        Pequeno,
+        Medio,
+        Grande
+    }
+
+    // Produto Concreto
+    public class VeiculoPequeno : Veiculo
+    {
+        public VeiculoPequeno(string modelo) : base(modelo, Porte.Pequeno)
+        {
+        }
+    }
+
+    // Produto Concreto
+    public class VeiculoMedio : Veiculo
+    {
+        public VeiculoMedio(string modelo) : base(modelo, Porte.Medio)
+        {
+        }
+    }
+
+    // Produto Concreto
+    public class VeiculoGrande : Veiculo
+    {
+        public VeiculoGrande(string modelo) : base(modelo, Porte.Grande)
+        {
+        }
+    }
+
+    public class VeiculoCreator
+    {
+        public static Veiculo Criar(string modelo, Porte porte)
+        {
+            switch (porte)
+            {
+                case Porte.Pequeno:
+                    return new VeiculoPequeno(modelo);
+                case Porte.Medio:
+                    return new VeiculoMedio(modelo);
+                case Porte.Grande:
+                    return new VeiculoGrande(modelo);
+                default:
+                    throw new ApplicationException("Porte de veiculo desconhecido.");
+            }
+        }
+    }
+}   
